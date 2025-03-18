57ae68996235b9a5c7602d125dcfdfa202d15606
Aliaa <aliaaalihesh@gmail.com>
2025-03-18 04:58:05 +0200

    teacher home and instructions

--- src/app/features/dashboard/teachers/teachers.routes.ts
+++ src/app/features/dashboard/teachers/teachers.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import { TeachersComponent } from './teachers.component';
 import { AttendanceComponent } from './attendance/attendance.component';
+import { HomeComponent } from './home/home.component';
+import { InstructionsComponent } from './instructions/instructions.component';
 
 
 export const teacherRoutes: Routes = [
   { path: 'teacher', component: TeachersComponent},
+  { path: 'home', component: HomeComponent},
   { path: 'attendance', component: AttendanceComponent },
-  
+  {path:'instructions', component: InstructionsComponent}
 ];
